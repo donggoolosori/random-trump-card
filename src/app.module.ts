4e92a216ad0570ac906481b885dff46781d81400
donggoolosori <kdj6551@naver.com>
2021-01-22 21:02:02 +0900

    Add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
