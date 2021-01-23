7bdb0112e23cb9c024a348b55abbe797fbe927cb
donggoolosori <kdj6551@naver.com>
2021-01-23 19:35:31 +0900

    Create card array

---
+++ src/CardArray.ts
@@ -0,0 +1,63 @@
+/*
+spade : s
+diamond : d
+club : c
+heart : h
+j,q,k,A : jack, queen, king, A spade
+joker : J1,J2
+*/
+const card = [
+  'J1',
+  'J2',
+  's2',
+  's3',
+  's4',
+  's5',
+  's6',
+  's7',
+  's8',
+  's9',
+  's10',
+  '*sj',
+  '*sq',
+  '*sk',
+  '*sa',
+  'd2',
+  'd3',
+  'd4',
+  'd5',
+  'd6',
+  'd7',
+  'd8',
+  'd9',
+  'd10',
+  '*dj',
+  '*dq',
+  '*dk',
+  'c2',
+  'c3',
+  'c4',
+  'c5',
+  'c6',
+  'c7',
+  'c8',
+  'c9',
+  'c10',
+  '*cj',
+  '*cq',
+  '*ck',
+  'h2',
+  'h3',
+  'h4',
+  'h5',
+  'h6',
+  'h7',
+  'h8',
+  'h9',
+  'h10',
+  '*hj',
+  '*hq',
+  '*hk',
+];
+
+export default card;
