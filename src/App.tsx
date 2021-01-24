9faf9511f42f71d299e643c141310713296b5de8
donggoolosori <kdj6551@naver.com>
2021-01-24 17:48:31 +0900

    Add count and skip to first,last

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import { Card } from './card/Card';
+// icon
+import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 const App = () => {
   const [cardSet, setCardSet] = useState(['J1']);
   const [cardIndex, setCardIndex] = useState(0);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     setCardSet(new Card().card);
@@ -12,14 +18,26 @@ const App = () => {
 
   const prevOnClick = () => {
     if (cardIndex > 0) {
+      setCount(count - 1);
       setCardIndex(cardIndex - 1);
     }
   };
 
   const nextOnClick = () => {
     if (cardIndex < 53) {
+      setCount(count + 1);
       setCardIndex(cardIndex + 1);
     }
+  };
+
+  const goFirst = () => {
+    setCount(1);
+    setCardIndex(0);
+  };
+
+  const goLast = () => {
+    setCount(54);
+    setCardIndex(53);
   };
 
   return (
@@ -31,13 +49,14 @@ const App = () => {
           alt="card"
         />
       </div>
+      <div className="counter">
+        <h2>{count}/54</h2>
+      </div>
       <div className="button-container">
-        <button className="btn prev" onClick={prevOnClick}>
-          Previous
-        </button>
-        <button className="btn next" onClick={nextOnClick}>
-          Next
-        </button>
+        <SkipPreviousIcon onClick={goFirst} />
+        <ArrowBackIosIcon onClick={prevOnClick} />
+        <ArrowForwardIosIcon onClick={nextOnClick} />
+        <SkipNextIcon onClick={goLast} />
       </div>
     </div>
   );
